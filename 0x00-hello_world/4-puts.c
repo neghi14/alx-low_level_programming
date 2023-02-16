18584a5cf04a556253d7d5704d5dba7266218711
neghi14 <sambasseynengimote@gmail.com>
2023-02-16 14:35:11 -0800

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,10 @@
  * puts - puts function from std io header
  *
  * Return: Always 0 (Success)
- * */
+ */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-};
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,10 +6,10 @@
  * printf - function to print std output
  *
  * Return: Always 0 (Success)
- * */
+ */
 
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
