e40596e6f20f616936ad7eacf32d256f94fa225b
neghi14 <sambasseynengimote@gmail.com>
2023-02-16 14:15:41 -0800

    Hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/*
+ *Create the main function
+ *
+ * Print String
+ * Return 0
+ * */
+
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art, \n");
 	return 0;
-};
+}
