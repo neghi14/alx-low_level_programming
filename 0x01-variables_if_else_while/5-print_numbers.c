fb55b87d79273ecfe0a3ae7e942591dfbabcb0ef
neghi14 <sambasseynengimote@gmail.com>
2023-02-17 13:46:56 -0800

    Variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ *
+ * Looping through using do while
+ *
+ * Return: Always 0 (Success)
+ */
+
+ int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+	return (0);
+
+}
