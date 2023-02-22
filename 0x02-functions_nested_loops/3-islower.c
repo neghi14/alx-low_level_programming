edea673ecf1e8808d1060c01c4e0b9cebe1d83ca
neghi14 <sambasseynengimote@gmail.com>
2023-02-21 16:41:31 -0800

    Functions

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *
+ * _islower - main entry
+ *
+ * Function to return 1 or 0 depending on answer;
+ *
+ * Return: 1 if c is lower and 0 if otherwise
+ *
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
