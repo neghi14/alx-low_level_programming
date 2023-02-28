9f08855c985164facc43dba72c194a2e747aeca7
neghi14 <sambasseynengimote@gmail.com>
2023-02-27 16:14:12 -0800

    Pointers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _puts - minic the stdio version of puts
  * @str: the value to process to the stdout.
@@ -7,7 +8,7 @@ void _puts(char *str)
 {
 	while (*str > 0)
 	{
-		_putchar(*str);
+		putchar(*str);
 		str++;
 	}
 	_putchar('\n');
